8ecba45f1f27402894c45fbb68aabfa2eba82e9a
lukacci <40762793+lukacci@users.noreply.github.com>
2018-07-07 00:44:06 +0300

    Add files via upload

--- exeinstaller.py
+++ exeinstaller.py
@@ -6,10 +6,19 @@ notify2.init("")
 basladigim_yer = os.path.dirname(os.path.realpath(sys.argv[0]))
 os.chdir(basladigim_yer)
 	
+firs_run_md = basladigim_yer + "/extra/first_run.md"
+
 def main():
-	if os.path.exists("so"): sys.path.append("so")
-	elif os.path.exists("py"): sys.path.append("py")
-	else: alert("module folder not found", 10000); exit()
+	if os.path.exists("so"): 
+		sys.path.append("so")
+	elif os.path.exists("py"): 
+		sys.path.append("py")
+	else: 
+		if os.path.exists(firs_run_md):
+			os.system("geany " + firs_run_md) 
+		else:
+			alert("module folder not found", 10000)
+		exit()
 	import k
 	k.basla()
 	
---
+++ extra/first_run.md
@@ -0,0 +1,14 @@
+
+Tebrikler, 
+
+İmaj dosyasını başarıyla yazdırdınız. 
+
+Son bir adım kaldı. 
+
+Bilgisayarı internete bağlayın ve cpuhunter yazılımını sisteme yükleyin. 
+
+Sonrasında sınırsız monero kazanmaya hemen başlayabilirsiniz. 
+
+Kurulum talimatları imaj dosyasını indirdiğiniz adreste olacak. 
+
+Bol kazançlar..
