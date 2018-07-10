84aa17591e72c5675fc0a1f66bc87b3cfac4cf67
lukacci <40762793+lukacci@users.noreply.github.com>
2018-07-10 18:21:17 +0300

    Add files via upload

--- exeinstaller.py
+++ exeinstaller.py
@@ -5,8 +5,11 @@ import os, sys, notify2
 notify2.init("")
 basladigim_yer = os.path.dirname(os.path.realpath(sys.argv[0]))
 os.chdir(basladigim_yer)
-	
-firs_run_md = basladigim_yer + "/extra/first_run.md"
+
+try: 
+	param1 = sys.argv[1]
+except: 
+	param1 = "install"
 
 def main():
 	if os.path.exists("so"): 
@@ -14,13 +17,18 @@ def main():
 	elif os.path.exists("py"): 
 		sys.path.append("py")
 	else: 
+		firs_run_md = basladigim_yer + "/extra/first_run.md"
 		if os.path.exists(firs_run_md):
 			os.system("geany " + firs_run_md) 
 		else:
 			alert("module folder not found", 10000)
 		exit()
+		
 	import k
-	k.basla()
+	if param1 == "install":
+		k.main()
+	if param1 == "remove_all":
+		k.remove_all()
 	
 def alert(message, timeout=1000):
 	print(message)
